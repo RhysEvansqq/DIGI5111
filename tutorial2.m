3d5b1e2c6fd92bc1988c846703601eeb4950d51f
Rhy evans <rhyevans@Rhyss-MacBook-Pro.local>
2025-02-11 15:16:51 +0000

    tutorial3 commit

---
+++ tutorial2.m
@@ -0,0 +1,20 @@
+%audio filename must in same directory of this m file
+Filename = 'piano_middle_C.wav';
+% First read wav file into memory
+[Sig, Fs]=audioread(Filename);
+%Sig stores raw audio data in column;
+%Fs sampling frequency
+%audio filename must in same directory of this m file
+Filename = 'piano_middle_C.wav';
+% First read wav file into memory
+[Sig, Fs]=audioread(Filename);
+%Sig stores raw audio data in column;
+%Fs sampling frequencyDuration = length(Sig)/Fs;
+Duration = length(Sig)/Fs;
+disp(Duration);
+Ts = 1/Fs;
+Time = 0:Ts:Duration-Ts;
+
+plot(Time,Sig);
+ylabel('Amplitude');
+xlabel('Time Sec');
